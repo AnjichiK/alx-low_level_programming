5d753271ab8a6b38bc015bb906053f0642886e23
AnjichiK <anjichik.ka@gmail.com>
2023-03-16 04:44:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
 
 /**
@@ -7,9 +8,8 @@
  * Return: Always 1.
  */
 
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
